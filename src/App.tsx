ee110eee0902e02354310bbd67896578672b907d
Victor Diaz <victordiaz@Victors-MacBook-Air.local>
2021-03-07 16:20:17 -0600

    add idea path to gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -3,41 +3,19 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedImageId, setSelectedImageId] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+    <div className="container">
+      <div>
+          <button
+              onClick={() => setSelectedImageId(selectedImageId - 1)
+              }
+          >
+              Previous
           </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+          <button>Next</button>
+      </div>
     </div>
   )
 }

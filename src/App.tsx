bce9f25a6b2c32ed7d9544fbc8d38c5b6bd640b7
Victor Diaz <victordiaz@Victors-MacBook-Air.local>
2021-03-07 16:29:52 -0600

    eslint config for standard with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  const [selectedImageId, setSelectedImageId] = useState(0)
+function App () {
+  const [selectedImageId, setSelectedImageId] = useState(0);
 
   return (
     <div className="container">

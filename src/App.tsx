d4bbeecb2943d359c6ba07ab70e4ff499ea94ebc
Victor Diaz <victordiaz@Victors-MacBook-Air.local>
2021-03-07 21:28:41 -0600

    it is working but not as expected

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,74 @@
 import React, { useState } from 'react'
-import './App.css'
+import classnames from 'classnames'
+import './App.scss'
+
+const pictures = [{
+  url: 'https://picsum.photos/id/874/900/600'
+}, {
+  url: 'https://picsum.photos/id/814/900/600'
+}, {
+  url: 'https://picsum.photos/id/1006/900/600'
+}]
+
+function getNextItemIndex (selectedItemIndex: number, itemsLength: number): number {
+  return (selectedItemIndex + 1) % itemsLength
+}
+
+function getPrevItemIndex (selectedItemIndex: number, itemsLength: number): number {
+  return (selectedItemIndex - 1 + itemsLength) % itemsLength
+}
+
+function isNextItem (selectedItemIndex: number, itemIndex: number): boolean{
+  return (selectedItemIndex + 1) === itemIndex
+}
+
+function isPrevItem (selectedItemIndex: number, itemIndex: number): boolean {
+  return (selectedItemIndex - 1) === itemIndex
+}
 
 function App (): JSX.Element {
-  const [selectedImageId, setSelectedImageId] = useState(0)
+  const [selectedImageIndex, setSelectedImageIndex] = useState<number>(0)
 
   return (
     <div className="container">
       <div>
-          <button
-            onClick={() => setSelectedImageId(selectedImageId - 1)}
-          >
-            Previous
-          </button>
-          <button
-            onClick={() => setSelectedImageId(selectedImageId + 1)}
-          >
-            Next
-          </button>
+        <div className='carousel-wrapper'>
+          <div className='carousel'>
+            {pictures.map((p, i) => (
+              <img
+                className={classnames(
+                  'carousel__item',
+                  i === 0 ? 'carousel__item-default' : '',
+                  i === selectedImageIndex ? 'carousel__item-active' : '',
+                  isPrevItem(selectedImageIndex, i) ? 'carousel__item-prev' : '',
+                  isNextItem(selectedImageIndex, i) ? 'carousel__item-next' : ''
+                )}
+                // I usually don't recommend to use indexes as key but in this
+                // particular case there are no risks involved
+                key={i}
+                src={p.url}
+              />
+            ))}
+          </div>
+        </div>
+        <button
+          onClick={() =>
+            setSelectedImageIndex(
+              getPrevItemIndex(selectedImageIndex, pictures.length)
+            )
+          }
+        >
+          Previous
+        </button>
+        <button
+          onClick={(ev) =>
+            setSelectedImageIndex(
+              getNextItemIndex(selectedImageIndex, pictures.length)
+            )
+          }
+        >
+          Next
+        </button>
       </div>
     </div>
   )

0382aeaa2371afe3ec25cefb8123ecaf8db5347e
Victor Diaz <victordiaz@Victors-MacBook-Air.local>
2021-03-09 10:09:14 -0600

    transition works using position absolute instead of changing from relative to absolut, this way we avoid to have images popup

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ const pictures = [{
   url: 'https://picsum.photos/id/814/900/600'
 }, {
   url: 'https://picsum.photos/id/1006/900/600'
+}, {
+  url: 'https://picsum.photos/id/1005/900/600'
 }]
 
 function getNextItemIndex (selectedItemIndex: number, itemsLength: number): number {
@@ -34,54 +36,72 @@ function isPrevItem (
   return getPrevItemIndex(selectedItemIndex, itemsLength) === itemIndex
 }
 
+enum MoveDirection {
+  stopped = 0,
+  left = 1,
+  right = 2,
+}
+
 function App (): JSX.Element {
   const [selectedImageIndex, setSelectedImageIndex] = useState<number>(0)
+  const [movingTo, setMovingto] = useState<MoveDirection>(MoveDirection.stopped)
 
   return (
     <div className="container">
       <div>
         <div className='carousel-wrapper'>
           <div className='carousel'>
-            {pictures.map((p, i) => (
-              <img
-                className={classnames(
-                  'carousel__item',
-                  // i === 0 ? 'carousel__item-default' : '',
-                  i === selectedImageIndex ? 'carousel__item-active' : '',
-                  isPrevItem(selectedImageIndex, i, pictures.length)
-                    ? 'carousel__item-prev'
-                    : '',
-                  isNextItem(selectedImageIndex, i, pictures.length)
-                    ? 'carousel__item-next'
-                    : ''
-                )}
-                // I usually don't recommend to use indexes as key but in this
-                // particular case there are no risks involved
-                key={i}
-                src={p.url}
-              />
-            ))}
+            <img
+              className='carousel__item-back'
+              src={pictures[0].url}
+            />
+            {pictures.map((p, i) => {
+              const isPrev = isPrevItem(selectedImageIndex, i, pictures.length)
+              const isNext = isNextItem(selectedImageIndex, i, pictures.length)
+              const shouldHide = (isPrev && movingTo === MoveDirection.left) ||
+                (isNext && movingTo === MoveDirection.right)
+              console.log('shoudl hide', shouldHide)
+              return (
+                <img
+                  className={classnames(
+                    'carousel__item',
+                    i === selectedImageIndex ? 'carousel__item-active' : '',
+                    isPrev ? 'carousel__item-prev' : '',
+                    isNext ? 'carousel__item-next' : '',
+                    shouldHide ? 'carousel__item-hide' : ''
+                  )}
+                  // I usually don't recommend to use indexes as key but in this
+                  // particular case there are no risks involved
+                  key={i}
+                  src={p.url}
+                />
+              )
+            })}
           </div>
         </div>
         <div>
           <button
-            onClick={() =>
+            onClick={() => {
               setSelectedImageIndex(
                 getPrevItemIndex(selectedImageIndex, pictures.length)
               )
-            }
+              setMovingto(MoveDirection.left)
+            }}
           >
             Previous
           </button>
           <button
-            onClick={(ev) =>
+            onClick={(ev) => {
               setSelectedImageIndex(
                 getNextItemIndex(selectedImageIndex, pictures.length)
               )
-            }
+              setMovingto(MoveDirection.right)
+            }}
           >
             Next
           </button>
+          <b>{selectedImageIndex}</b>
+          <b>{movingTo}</b>
         </div>
         <div>
           <h1>title</h1>

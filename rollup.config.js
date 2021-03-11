298a953e366950b25c0a2e5387d7acb0566c1e29
Victor Diaz <victordiaz@Victors-MacBook-Air.local>
2021-03-10 21:00:03 -0600

    add support for sass file

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
 import visualizer from 'rollup-plugin-visualizer'
+import scss from 'rollup-plugin-scss'
 
 import pkg from './package.json'
 import typescript from '@rollup/plugin-typescript'
@@ -20,6 +21,7 @@ export default {
     typescript(),
     resolve({ extensions: ['js', 'ts', 'tsx'] }),
     commonjs(),
+    scss(),
     visualizer()
   ]
 }
